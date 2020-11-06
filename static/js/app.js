f96a039489d1af38726f149ad9d3c93627b5b878
Eric S Penrose <ericspenrose@gmail.com>
2020-11-06 12:49:17 -0600

    Got it working!

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,38 @@
 // from data.js
-var tableData = data;
+var tableData = Object.assign(data);
 
-// YOUR CODE HERE!
+var tbody = d3.select('tbody')
+
+function buildTable(tdata) {
+    tdata.forEach((datum) => {
+        var row = tbody.append("tr");
+        Object.entries(datum).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+}
+buildTable(tableData)
+// datetime Form:
+var datetime = d3.select("#datetime")
+// datetime.on("change", function () {
+//     var inputDate = d3.event.target.value
+//     console.log(inputDate)
+// })
+function datetimeFilter(entry) {
+    return entry.datetime == inputDate
+}
+
+//Filter Button:
+var filter = d3.select('#filter-btn')
+filter.on("click", function () {
+    d3.event.preventDefault()
+    d3.selectAll("tr").remove()
+    inputDate = datetime.property("value")
+    tableData = data.filter(datetimeFilter)
+    buildTable(tableData)
+})
+
+
+
+
