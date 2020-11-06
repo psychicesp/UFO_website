fbdc08e47f9c56cc261e52f23f4add16a01a73ec
Eric S Penrose <ericspenrose@gmail.com>
2020-11-06 13:13:01 -0600

    BONUS!!

--- static/js/app.js
+++ static/js/app.js
@@ -15,22 +15,63 @@ function buildTable(tdata) {
 buildTable(tableData)
 // datetime Form:
 var datetime = d3.select("#datetime")
-// datetime.on("change", function () {
-//     var inputDate = d3.event.target.value
-//     console.log(inputDate)
-// })
 function datetimeFilter(entry) {
     return entry.datetime == inputDate
+}
+
+// city Form:
+var city = d3.select("#city")
+function cityFilter(entry) {
+    return entry.city == inputCity
+}
+
+// state Form:
+var state = d3.select("#state")
+
+function stateFilter(entry) {
+    return entry.state == inputState
+}
+
+// country Form:
+var country = d3.select("#country")
+function countryFilter(entry) {
+    return entry.country == inputCountry
+}
+
+// shape Form:
+var shape = d3.select("#shape")
+function shapeFilter(entry) {
+    return entry.shape == inputShape
 }
 
 //Filter Button:
 var filter = d3.select('#filter-btn')
 filter.on("click", function () {
+    if (datetime.property("value")){
+        inputDate = datetime.property("value")
+        tableData = tableData.filter(datetimeFilter)
+    }
+    if (city.property("value")){
+        inputCity = city.property("value")
+        tableData = tableData.filter(cityFilter)
+    }
+    if (state.property("value")){
+        inputState = state.property("value")
+        tableData = tableData.filter(stateFilter)
+    }
+    if (country.property("value")){
+        inputCountry = country.property("value")
+        tableData = tableData.filter(countryFilter)
+    }
+    if (shape.property("value")){
+        inputShape = shape.property("value")
+        tableData = tableData.filter(shapeFilter)
+    }
     d3.event.preventDefault()
     d3.selectAll("tr").remove()
     inputDate = datetime.property("value")
-    tableData = data.filter(datetimeFilter)
     buildTable(tableData)
+    tableData = data
 })
 
 
